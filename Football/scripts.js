1f7b5451fd2b2e910ae77274df7ed757117f3a8b
A1CodeBreaker <129992490+A1CodeBreaker@users.noreply.github.com>
2023-05-10 15:54:30 -0400

    Update scripts.js

--- Football/scripts.js
+++ Football/scripts.js
@@ -1,21 +1,40 @@
-console.log("JS working");
 let teams = [
-  { code: "DAL", name: "Dallas Cowboys", plays: "Arlington, TX" },
-  { code: "DEN", name: "Denver Broncos", plays: "Denver, CO" },
-  { code: "HOU", name: "Houston Texans", plays: "Houston, TX" },
-  { code: "KAN", name: "Kansas City Chiefs", plays: "Kansas City, MO" },
-];
+    { code: 'DAL', name: 'Dallas Cowboys', plays: 'Arlington, TX' },
+    { code: 'DEN', name: 'Denver Broncos', plays: 'Denver, CO' },
+    { code: 'HOU', name: 'Houston Texans', plays: 'Houston, TX' },
+    { code: 'KAN', name: 'Kansas City Chiefs', plays: 'Kansas City, MO' },
+  ];
+   const teamSelectorEl = document.getElementById('teamSelector');
 
-function fillSelect(teamList) {
-  console.log("fillSelect Started");
-  const selectTeamEl = document.getElementById("teamList");
-  for (let i in teamList) {
-    const teamName = teamList[i].name;
-    const teamVal = teamList[i].code;
-
-    const optionsEl = new Option(teamName, teamVal);
-    selectTeamEl.appendChild(optionsEl);
-    console.log("fillselect Finished");
+  function playerDropDown() {
+    const teamSelectorEl = document.getElementById('teamSelector');
+    for (let i = 0; i < teams.length; i++) {
+      let optionEl = document.createElement('option');
+      const team = teams[i];
+      optionEl.textContent = team.name;
+      optionEl.value = team.code;
+      teamSelectorEl.appendChild(optionEl);
+    }
   }
-}
-fillSelect(teams);
+  
+  playerDropDown();
+
+  function onButtonClick() {
+    const teamCode = teamSelectorEl.value;
+    console.log('selected value', teamCode);
+
+    let selectedTeam;
+    for (let i = 0; i < Teams.length; i++) {
+    const team = teams[i];
+        if (team.code === teamCode) {
+            selectedTeam = team;
+            break;
+        }
+    }
+    const message = 'You selected the ${selectedTeam.name} (${selectedTeam.code)}'
+    const messageEl = document.getElementById('messagep');
+    messageEl.innerHTML = message;
+  }
+
+  const btn = document.getElementById('buttonInput');
+  btn.onclick = onButtonClick;
